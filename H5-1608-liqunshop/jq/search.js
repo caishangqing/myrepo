90270c3293d378714c45f6132de4e508dc096299
caishangqing <121605366@qq.com>
2016-09-06 22:08:37 +0800

    9/6更新

--- H5-1608-liqunshop/jq/search.js
+++ H5-1608-liqunshop/jq/search.js
@@ -4,6 +4,13 @@
 $(function(){
 
 
+$("img.lazy").lazyload({
+	effect: "fadeIn",
+	placeholder : "../imgs/search-img/wait2.gif",
+	threshold: 200
+});
+
+
 $(".item_title").click(function(){
 	var $tar=$(this).next(".item_list");
 	if($tar.is(":visible")){
@@ -12,10 +19,8 @@ $(".item_title").click(function(){
 	}else{
 		$tar.stop(true,false).slideDown(400);
 		$(this).children("i").css("background-position","0 0");
-
 	}
 })
-
 
 
 
